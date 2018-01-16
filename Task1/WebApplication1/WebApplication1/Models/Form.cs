2d3d27c0238adc9c6ffaaee385e4ed8bedd23b04
VyachesalvAkulov <slavikshark1919@gmail.com>
2018-01-17 01:54:31 +0200

    Task1_add

---
+++ Task1/WebApplication1/WebApplication1/Models/Form.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Sex { get; set; }
+        public int Age { get; set; }
+
+        public string Team { get; set; }
+
+        public int Position { get; set; }
+
+        public int Salary { get; set; }
+    }
+   
+}
---
+++ Task1/WebApplication1/WebApplication1/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Text { get; set; }
+       
+    }
+   
+}
