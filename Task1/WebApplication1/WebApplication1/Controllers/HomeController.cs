30d9525ef38ce99458e959f500c2f770613a5cdc
VyachesalvAkulov <slavikshark1919@gmail.com>
2018-01-17 02:11:10 +0200

    TAsks

---
+++ Task1/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBContext context = new DBContext();
+        public ActionResult Index()
+        {
+            ViewBag.Header = "Main Paage";
+            return View(model: context.Notes);
+        }
+        public ActionResult Form(Form form = null)
+        {
+            ViewBag.Header = "Form page";
+            if (Request.HttpMethod == "GET")
+            {
+                return View();
+            }
+            if (Request.HttpMethod == "POST")
+            {
+                if (form != null)
+                {
+                    context.Forms.Add(form);
+                    context.SaveChanges();
+                    return View("FormComplete", form);
+                }
+
+            }
+            return HttpNotFound();
+        }
+        [HttpGet]
+        public ActionResult Guestbook()
+        {
+            ViewBag.Header = "Guestbook page";
+            return View(model: context.Reviews);
+        }
+        [HttpPost]
+        public ActionResult Guestbook(Review review)
+        {
+            review.Date = DateTime.Now;
+            context.Reviews.Add(review);
+            context.SaveChanges();
+            return RedirectToAction("Guestbook");
+        }
+        public PartialViewResult GuestbookPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FormComplete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task1/WebApplication1/WebApplication1/Models/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("DefaultConnection")
+        { }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+    }
+}
